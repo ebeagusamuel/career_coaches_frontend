4dc57e60ea4000608ca5cb5edb4c27f0225e85f3
ebeagusamuel <samuelebeagu@gmail.com>
2020-11-26 03:42:07 +0100

    add async method to create and cancel appointments

--- src/CareerCoaches/coachesSlice.js
+++ src/CareerCoaches/coachesSlice.js
@@ -30,17 +30,38 @@ export const fetchAppointments = createAsyncThunk('careerCoaches/fetchAppointmen
   return data;
 });
 
+export const bookAppointment = createAsyncThunk('careerCoaches/bookAppointment', async (token, appointmentDetail) => {
+  const response = await fetch('http://localhost:3001/book_appointment/', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(appointmentDetail),
+  });
+  const data = await response.json();
+
+  return data;
+});
+
+export const cancelAppointment = createAsyncThunk('careerCoaches/cancelAppointment', async (token, coachId) => {
+  const response = await fetch('http://localhost:3001/cancel_appointment/', {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(coachId),
+  });
+  const data = await response.json();
+
+  return data;
+});
+
 const coachesSlice = createSlice({
   name: 'careerCoaches',
   initialState,
-  reducers: {
-    // logout: (state, action) => {
-    //   const newState = state;
-    //   window.localStorage.removeItem('userObj');
-    //   newState.loggedIn = action.payload;
-    //   return newState;
-    // },
-  },
+  reducers: {},
   extraReducers: {
     [fetchCoachesObj.pending]: state => {
       const newState = state;
@@ -73,6 +94,46 @@ const coachesSlice = createSlice({
       return newState;
     },
     [fetchAppointments.rejected]: (state, action) => {
+      const newState = state;
+      newState.status = 'rejected';
+      newState.error = action.error;
+      return newState;
+    },
+    [bookAppointment.pending]: state => {
+      const newState = state;
+      newState.status = 'loading';
+      return newState;
+    },
+    [bookAppointment.fulfilled]: (state, action) => {
+      const newState = state;
+      newState.status = 'fulfilled';
+      if (action.payload.error) {
+        newState.error = action.payload.error;
+        return newState;
+      }
+      newState.message = action.payload.message;
+      newState.error = null;
+      return newState;
+    },
+    [bookAppointment.rejected]: (state, action) => {
+      const newState = state;
+      newState.status = 'rejected';
+      newState.error = action.error;
+      return newState;
+    },
+    [cancelAppointment.pending]: state => {
+      const newState = state;
+      newState.status = 'loading';
+      return newState;
+    },
+    [cancelAppointment.fulfilled]: (state, action) => {
+      const newState = state;
+      newState.status = 'fulfilled';
+      newState.message = action.payload.message;
+      newState.error = null;
+      return newState;
+    },
+    [cancelAppointment.rejected]: (state, action) => {
       const newState = state;
       newState.status = 'rejected';
       newState.error = action.error;
