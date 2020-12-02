404c0e662ef77dee1daf6df47e26d150ec67a88c
ebeagusamuel <samuelebeagu@gmail.com>
2020-12-02 16:32:55 +0100

    add test to check if the app renders without any problem

---
+++ src/tests/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import App from '../CareerCoaches/App';
+import store from '../store';
+
+
+test('renders without crashing', () => {
+  const { baseElement } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
+  expect(baseElement).toBeDefined();
+});
